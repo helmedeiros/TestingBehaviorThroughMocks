6f8ae601827791b5fa291e86a4b87fc7afa42eba
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-09-28 08:22:01 -0300

    Added a new test that verifies if one or two days old' auctions were closed or not after closing batch request.

--- src/test/java/br/com/caelum/auction/service/AuctionCloserTest.java
+++ src/test/java/br/com/caelum/auction/service/AuctionCloserTest.java
@@ -22,38 +22,63 @@ import static org.mockito.Mockito.when;
  */
 public class AuctionCloserTest {
 
+    public static final String VALID_AUCTION_NAME = "PLAYSTATION 3";
+    public static final int SEVEN_DAYS_AGO = -7;
+    public static final int FOURTEEN_DAYS_AGO = -14;
+    public static final int ONE_DAY_AGO = -1;
+    public static final int TWO_DAYS_AGO = -2;
+    public static final boolean IS_NOT_CLOSED = false;
+    public static final boolean IS_CLOSED = true;
+
     @Test public void shouldCloseAuctionsBeganMoreThanWeekAgo() throws Exception {
         AuctionDao auctionDaoMock = mock(AuctionDao.class);
 
-        Calendar twoWeeksAgoDate = Calendar.getInstance();
-        twoWeeksAgoDate.add(Calendar.DAY_OF_MONTH, -14);
+        Calendar twoWeeksAgoDate = giveMeDateFrom(FOURTEEN_DAYS_AGO);
+        Calendar oneWeekAgo = giveMeDateFrom(SEVEN_DAYS_AGO);
 
-        Calendar oneWeekAgo = Calendar.getInstance();
-        oneWeekAgo.add(Calendar.DAY_OF_MONTH, -7);
-
-        Calendar thisWeek = Calendar.getInstance();
-        oneWeekAgo.add(Calendar.DAY_OF_MONTH, -1);
-
-        final Auction openAuction1 = new AuctionBuilder().to("PLAYSTATION 3").onDate(twoWeeksAgoDate).build();
-        final Auction openAuction2 = new AuctionBuilder().to("PLAYSTATION 3").onDate(oneWeekAgo).build();
-        final Auction openAuction3 = new AuctionBuilder().to("PLAYSTATION 3").onDate(thisWeek).build();
+        final Auction openAuction1 = createAuctionAndAssertItIs(VALID_AUCTION_NAME, twoWeeksAgoDate, IS_NOT_CLOSED);
+        final Auction openAuction2 = createAuctionAndAssertItIs(VALID_AUCTION_NAME, oneWeekAgo, IS_NOT_CLOSED);
 
         List<Auction> openAuctionList = Arrays.asList(openAuction1, openAuction2);
         when(auctionDaoMock.actuals()).thenReturn(openAuctionList);
 
         final AuctionCloser auctionCloser = new AuctionCloser(auctionDaoMock);
 
-        assertThat(openAuction1.isClosed(), equalTo(false));
-        assertThat(openAuction2.isClosed(), equalTo(false));
-        assertThat(openAuction3.isClosed(), equalTo(false));
-
         auctionCloser.close();
 
         assertThat(auctionCloser.getClosedTotal(), equalTo(2));
-        assertThat(openAuction1.isClosed(), equalTo(true));
-        assertThat(openAuction2.isClosed(), equalTo(true));
-        assertThat(openAuction3.isClosed(), equalTo(false));
+        assertThat(openAuction1.isClosed(), equalTo(IS_CLOSED));
+        assertThat(openAuction2.isClosed(), equalTo(IS_CLOSED));
     }
 
+    @Test public void shouldNotCloseAuctionsBeganLessThanWeekAgo() throws Exception {
 
+        final Calendar yesterday = giveMeDateFrom(ONE_DAY_AGO);
+        final Calendar beforeYesterday = giveMeDateFrom(TWO_DAYS_AGO);
+
+        final Auction openAuctionFromYesterday = createAuctionAndAssertItIs(VALID_AUCTION_NAME, yesterday, IS_NOT_CLOSED);
+        final Auction openAuctionFromBeforeYesterday = createAuctionAndAssertItIs(VALID_AUCTION_NAME, beforeYesterday, IS_NOT_CLOSED);
+
+        AuctionDao mockDao = mock(AuctionDao.class);
+        when(mockDao.actuals()).thenReturn(Arrays.asList(openAuctionFromYesterday, openAuctionFromBeforeYesterday));
+
+        final AuctionCloser auctionCloser = new AuctionCloser(mockDao);
+        auctionCloser.close();
+
+        assertThat(auctionCloser.getClosedTotal(), equalTo(0));
+        assertThat(openAuctionFromYesterday.isClosed(), equalTo(IS_NOT_CLOSED));
+        assertThat(openAuctionFromBeforeYesterday.isClosed(), equalTo(IS_NOT_CLOSED));
+
+    }
+
+    private Auction createAuctionAndAssertItIs(final String auctionName, final Calendar onDate, final boolean closed) {
+        final Auction openAuctionFromYesterday = new AuctionBuilder().to(auctionName).onDate(onDate).build();
+        assertThat(openAuctionFromYesterday.isClosed(), equalTo(closed));
+        return openAuctionFromYesterday;
+    }
+
+    private Calendar giveMeDateFrom(final int dayAgo) {
+        final Calendar expectedDate = Calendar.getInstance(); expectedDate.add(Calendar.DAY_OF_MONTH, dayAgo);
+        return expectedDate;
+    }
 }
